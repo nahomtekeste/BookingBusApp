b96c2784161f08ad90cc7b3d9f0228bbbec74948
nahomtekeste <46280184+nahomtekeste@users.noreply.github.com>
2019-12-01 14:38:37 +0000

    Initial commit

---
+++ app/src/main/java/com/example/bookingbusapp/Main3Activity.kt
@@ -0,0 +1,12 @@
+package com.example.bookingbusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
